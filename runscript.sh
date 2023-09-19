2a2d9372a163140b96c32f12d42201178b5e3b01
Sagar <guptas10@chop.edu>
2023-09-19 17:36:55 -0400

    temp

--- runscript.sh
+++ runscript.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
+#SBATCH --job-name=test
+#SBATCH --time=6:00:00
+#SBATCH -p gpuq
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH -o outfile.out
+#SBATCH --error=./error.out
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/isilon/sgourakis_lab_storage/anaconda3/envs/alphafold/lib:/mnt/isilon/sgourakis_lab_storage/anaconda3/envs/alphafold/lib/python3.9/site-packages/tensorrt
 
-#source /mnt/isilon/sgourakis_lab_storage/anaconda3/bin/activate
-#conda activate alphafold
+source /mnt/isilon/sgourakis_lab_storage/anaconda3/bin/activate
+conda activate alphafold
 
 for targets in ./input_sequenceblind/*.txt; do
 	targname=$(echo ${targets} | cut -f 3 -d '/' | cut -f 1 -d '_')
