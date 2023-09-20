8fd5597291065efc8f13cfecd2a14ec8ea7659cf
Sagar Gupta <sagarg@sas.upenn.edu>
2023-09-19 20:16:11 -0400

    temp

--- runscript.sh
+++ runscript.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=64G
 #SBATCH -o outfile.out
 #SBATCH --error=./error.out
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/isilon/sgourakis_lab_storage/anaconda3/envs/alphafold/lib:/mnt/isilon/sgourakis_lab_storage/anaconda3/envs/alphafold/lib/python3.9/site-packages/tensorrt
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/isilon/sgourakis_lab_storage/anaconda3/envs/alphafold/lib:/mnt/isilon/sgourakis_lab_storage/anaconda3/envs/alphafold/lib/python3.9/site-packages/tensorrt
 source /mnt/isilon/sgourakis_lab_storage/anaconda3/bin/activate
 conda activate alphafold
 
@@ -15,8 +15,6 @@ for targets in ./input_seq/*.txt; do
 	targname=$(echo ${targets} | cut -f 3 -d '/' | cut -f 1 -d '_')
 	echo ${targname}
 
-	python run_prediction.py --targets ${targname}/inputs/target.tsv --outfile_prefix ${targname}/outfile --model_names model_2_ptm_ft --model_params_files params/7WKJ_af_mhc_params_2351.pkl  --ignore_identities
+	python run_prediction.py --targets ${targname}/inputs/target.tsv --outfile_prefix ${targname}/outfile --model_names model_2_ptm_ft --model_params_files /mnt/isilon/sgourakis_lab_storage/persona/sagar/afft_hla3db/params/7WKJ_af_mhc_params_2351.pkl  --ignore_identities
 
-#	end_time=`date +%s`
-#	echo Execution_time_in seconds, `expr $end_time - $start_time`>>time_data/time_${targname}.csv
 done
