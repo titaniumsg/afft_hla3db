7dd3bd88d905610d1412a68728cd511b475a3bee
Sagar <guptas10@chop.edu>
2023-09-19 20:18:56 -0400

    temp

--- runscript.sh
+++ runscript.sh
@@ -15,6 +15,6 @@ for targets in ./input_seq/*.txt; do
 	targname=$(echo ${targets} | cut -f 3 -d '/' | cut -f 1 -d '_')
 	echo ${targname}
 
-	python run_prediction.py --targets ${targname}/inputs/target.tsv --outfile_prefix ${targname}/outfile --model_names model_2_ptm_ft --model_params_files /mnt/isilon/sgourakis_lab_storage/persona/sagar/afft_hla3db/params/7WKJ_af_mhc_params_2351.pkl  --ignore_identities
+	python run_prediction.py --targets ${targname}/inputs/target.tsv --outfile_prefix ${targname}/outfile --model_names model_2_ptm_ft --model_params_files /mnt/isilon/sgourakis_lab_storage/personal/sagar/afft_hla3db/params/7WKJ_af_mhc_params_2351.pkl  --ignore_identities
 
 done
